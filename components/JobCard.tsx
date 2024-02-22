379918b00fff7a53727007a55a4de79d9c374f12
tdoval <thiagodovalaraujo@gmail.com>
2024-02-22 19:22:39 -0300

    JobCard

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { JobType } from "@/utils/types";
+import { JobMode, JobType } from "@/utils/types";
 import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 
 import Link from "next/link";
@@ -18,13 +18,24 @@ import JobInfo from "./JobInfo";
 import DeleteJobButton from "./DeleteJobButton";
 
 function JobCard({ job }: { job: JobType }) {
+    const date = new Date(job.createdAt).toLocaleDateString();
     return (
         <Card className="bg-muted">
             <CardHeader>
                 <CardTitle>{job.position}</CardTitle>
                 <CardDescription>{job.company}</CardDescription>
                 <Separator />
-                <CardContent></CardContent>
+                <CardContent className="mt-4 grid grid-cols-2 gap-4">
+                    <JobInfo icon={<Briefcase />} text={job.mode} />
+                    <JobInfo icon={<MapPin />} text={job.location} />
+                    <JobInfo icon={<CalendarDays />} text={date} />
+                    <Badge className="w-32 justify-center">
+                        <JobInfo
+                            icon={<RadioTower className="w-4 h-4" />}
+                            text={job.status}
+                        />
+                    </Badge>
+                </CardContent>
                 <CardFooter className="flex gap-4">
                     <Button asChild size="sm">
                         <Link href={`/jobs/${job.id}`}>Edit</Link>
