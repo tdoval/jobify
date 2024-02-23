fc30fc96f8da34843cd5f0f20da751c226caa1c6
tdoval <thiagodovalaraujo@gmail.com>
2024-02-23 18:11:53 -0300

    EditJobForm component

--- utils/actions.ts
+++ utils/actions.ts
@@ -132,3 +132,25 @@ export async function getSingleJobAction(id: string): Promise<JobType | null> {
     }
     return job;
 }
+
+export async function updateJobAction(
+    id: string,
+    values: CreateAndEditJobType
+): Promise<JobType | null> {
+    const userId = authenticateAndRedirect();
+
+    try {
+        const job: JobType = await prisma.job.update({
+            where: {
+                id,
+                clerkId: userId,
+            },
+            data: {
+                ...values,
+            },
+        });
+        return job;
+    } catch (error) {
+        return null;
+    }
+}
